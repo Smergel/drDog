b6191aeb1183024d01ea252a6440d8aa58b75020
William Bolton <william.ellet@gmail.com>
2015-06-26 10:15:25 -0400

    fist commit of friday

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   console.log("Document is ready, sucka!")
 
   $("nav ul li").hover(function() {
-    $("#toggle + label").toggle(400, "linear");
+    $("#toggle + label").toggle();
   });
 });
